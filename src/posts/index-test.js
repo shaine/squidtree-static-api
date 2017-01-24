a076f083b6c25b27e7b8a1763b6ffaa6202fb156
Shaine Hatch <shainehatch@overstock.com>
2017-01-24 15:24:08 -0600

    feat(post): Denormalize post object with user

--- src/posts/index-test.js
+++ src/posts/index-test.js
@@ -1,8 +1,12 @@
 const expect = require('chai').expect;
 const {
+    _getDenormalizedPost,
     _getPosts,
     _getPostsByPage
 } = require('./index');
+const {
+    _getUserById
+} = require('../users');
 const state = require('../../testDb.json');
 
 describe('posts', () => {
@@ -11,6 +15,17 @@ describe('posts', () => {
             expect(_getPosts(state))
                 .to.exist
                 .to.equal(state.posts);
+        });
+    });
+
+    describe('getDenormalizedPost', () => {
+        it('denormalizes a post entity', () => {
+            expect(_getDenormalizedPost(state, {
+                user_id: 1
+            })).to.eql({
+                user_id: 1,
+                user: _getUserById(state, 1)
+            });
         });
     });
 
--- src/posts/index.js
+++ src/posts/index.js
@@ -1,4 +1,7 @@
 const { createSelector } = require('reselect');
+const {
+    _getUserById
+} = require('../users');
 
 const DEFAULT_PAGE_SIZE = 20;
 
@@ -7,6 +10,13 @@ const getPosts = createSelector(
     state => state.posts
 );
 exports._getPosts = getPosts;
+
+const getDenormalizedPost = createSelector(
+    (state, post) => _getUserById(state, post.user_id),
+    (state, post) => post,
+    (user, post) => Object.assign({}, post, { user })
+);
+exports._getDenormalizedPost = getDenormalizedPost;
 
 const getPostsByPage = createSelector(
     getPosts,
